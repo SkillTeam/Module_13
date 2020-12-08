eb30ca56777aa54280f15596541ba4e9121a543e
fg-yarullin <66568263+fg-yarullin@users.noreply.github.com>
2020-12-08 17:57:04 +0300

    Update tikTakBoom.js

--- js/tikTakBoom.js
+++ js/tikTakBoom.js
@@ -14,7 +14,7 @@ tikTakBoom = {
         countDownId,
         questionDelayId,
     ) {
-        this.countOfPlayers = 2;
+        this.countOfPlayers = 3;
         this.timerField = null;
         this.timerFields = [undefined];
         this.counterField = counterField;

546910fbb9c14da205ec70bd27a1e541dbc68a3e
fg-yarullin <66568263+fg-yarullin@users.noreply.github.com>
2020-12-08 17:49:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 window.onload = function()
 {
     var timerId = null;
-    //var nextQuestion = true;
     tikTakBoom.init(
         tasks,
-        // document.getElementById('timerField'),
         document.getElementById('counterField'),
         document.getElementById('gameStatusField'),
-        document.getElementById('questionField'),
-        document.getElementById('answer1'),
-        document.getElementById('answer2'),
-        document.getElementById('answer3'),
+        // document.getElementById('questionField'),
+        // document.getElementById('answer1'),
+        // document.getElementById('answer2'),
+        // document.getElementById('answer3'),
         document.getElementById('answerFields'),
         document.getElementById('startGameBtn'),
+        document.getElementById('finishGameBtn'),
         timerId,
-        //nextQuestion,
-        // document.getElementById('finishGameBtn'),
     )
     tikTakBoom.run();
-    //document.getElementById('answerFields').setAttribute('disabled', 'disabled');
 }
